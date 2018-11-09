ab4e5c85b1d208e13a1f23fb44944ebaae22cedc
Blaz <mb250653@ncr.com>
2018-11-09 05:05:13 +0100

    Building the project
    - How to use  a realWorkld API
    - What API keys are and why we need them.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,19 @@
-import str from './models/Search';
-//import {add as a, multiply as m, ID } from './views/searchView';
-import *  as searchView from './views/searchView';
-console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${searchView.str}`);
+import axios from 'axios';
+
+async function getResults(query) {
+    const proxy = `https://cors-anywhere.herokuapp.com/`;
+    const key = '4fe8e6d4cf18c9028fc403fc23a705dc';
+    try{
+        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes)
+    }catch (error){
+        alert(error);
+    }
+}
+    
+getResults('tomato pasta');
+
+
+// API Key: 4fe8e6d4cf18c9028fc403fc23a705dc
+// API Search: https://www.food2fork.com/api/search
