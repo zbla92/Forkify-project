1652906886bb8884bddbd06f6df7cfd2af96e08e
Blaz <mb250653@ncr.com>
2018-11-09 04:34:25 +0100

    beginning
    - How to use ES6 modules;
    - Default and named exports and imports

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import str from './models/Search';
+//import {add as a, multiply as m, ID } from './views/searchView';
+import *  as searchView from './views/searchView';
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${searchView.str}`);
