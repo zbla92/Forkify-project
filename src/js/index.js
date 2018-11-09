c62f576224bc367cb1137e3c9098705a35cb2d03
zbla92 <34164504+zbla92@users.noreply.github.com>
2018-11-10 00:17:10 +0100

    Revert "build 0.9.149"
    This reverts commit a0e05e52b022d4840e35a14c5fd29689d29252dc.

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,19 @@
-import Search from './models/Search';
+import axios from 'axios';
 
-/**Global state of the app
- * Search object
- * current recipe object
- * SHopping list object
- * like recipes
- *  
- */
-const state = {};
-
-const controlSearch = async () => {
-    // 1) Get query from view
-    const query = 'pizza' //TODO 
-
-    if (query) {
-        // 2) New search object and add to state
-        state.search = new Search(query);
-
-        // 3) Prepare UI for reslts
-
-        // 4) Search for recipes
-        await state.search.getResults();
-
-        // 5) Render results on UI
-        console.log(state.search.result);
-        }
+async function getResults(query) {
+    const proxy = `https://cors-anywhere.herokuapp.com/`;
+    const key = '4fe8e6d4cf18c9028fc403fc23a705dc';
+    try{
+        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes)
+    }catch (error){
+        alert(error);
+    }
 }
+    
+getResults('tomato pasta');
 
 
-document.querySelector('.search').addEventListener('submit', e => {
-    e.preventDefault();
-    controlSearch();
-});
+// API Key: 4fe8e6d4cf18c9028fc403fc23a705dc
+// API Search: https://www.food2fork.com/api/search
