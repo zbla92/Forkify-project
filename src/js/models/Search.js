ce9b0feeaf90cf9fc6127480f207b368c3ede536
zbla92 <34164504+zbla92@users.noreply.github.com>
2018-11-11 14:08:02 +0100

    Building the search view
    1 - Advanced DOM maniulation techniques;
    2- How to use ES6 template stings to rencer enrite HtmL components;
    3 - How to create a loading spinner

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,37 +1,19 @@
 import axios from 'axios'
 
-
-
 export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
 
-constructor(query) {
-
-this.query = query;
-
-}
-
-
-
-async getResults() {
-
-const key = 'd2e967cc683df1e92aa4480165ce26c1';
-
-const proxy = 'https://cors-anywhere.herokuapp.com/';
-
-try {
-
-const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
-
-this.result = res.data.recipes;
-
-//console.log(result);
-
-} catch(error) {
-
-alert(error);
-
-}
-
-}
-
+    async getResults() {
+        const key = 'd2e967cc683df1e92aa4480165ce26c1';
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        try {
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            //console.log(result);
+        } catch (error) {
+            alert(error);
+        }
+    }
 }
